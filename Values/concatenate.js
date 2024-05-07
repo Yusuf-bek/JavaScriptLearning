1bd58cae5fe9e5e0a101dc1d57be3c1608417466
Yusuf-bek <ogabeksherakhmatov@gmail.com>
2024-05-07 09:41:37 +0500

    concatenating

---
+++ Values/concatenate.js
@@ -0,0 +1,7 @@
+"con" + "cat" + "e" + "nate";
+// Ther operator + is not actually doing arithmetic thing but it is 
+// simply `concatenating` them
+
+console.log("this" + " is" + " just" + " ConCatenating");
+
+// Strings in Javascript cannot be divided, subtracted or multiplied
