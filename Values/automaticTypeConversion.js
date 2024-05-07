f0fa6b59010390bb00067a563478cd4f4ed4a07c
Yusuf-bek <ogabeksherakhmatov@gmail.com>
2024-05-07 18:21:25 +0500

    automaticTypeConversions

---
+++ Values/automaticTypeConversion.js
@@ -0,0 +1,32 @@
+console.log(8 * null)
+// → 0
+console.log("5" - 1)
+// → 4
+console.log("5" + 1)
+// → 51
+console.log("five" * 2)
+// → NaN
+console.log(false == 0)
+// → true
+
+// when operator applied in wrong way 
+// Javascript automatically converts to other type
+
+console.log('3' - 2)
+console.log("3.3" + 5)
+
+// this might lead to major complexity or problem
+// but anyways who cares. This is JavaScript and there are plenty of jobs 
+// works that i can do with it. So nothing is perfect
+
+console.log("i might find" - 3)
+console.log("nine" * 2)
+
+
+// but i really liked this one
+
+console.log(false == 0)
+console.log(true == 1)
+
+// first time i have seen it in python
+// which languages converts 0 to false and 1 to true when it is needed
