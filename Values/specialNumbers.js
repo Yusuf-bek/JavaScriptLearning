0f3753885be2d64175b13ee5aac831dce505f523
Yusuf-bek <ogabeksherakhmatov@gmail.com>
2024-05-07 09:22:56 +0500

    special Numbers in javascript

---
+++ Values/specialNumbers.js
@@ -0,0 +1,10 @@
+// Ther are 3 special numbers in Javascript 
+
+Infinity;
+// Just math terms
+-Infinity;
+
+
+NaN
+// NaN is 'not a number' 
+// it occurs usually by some operation with Infinity and etc
