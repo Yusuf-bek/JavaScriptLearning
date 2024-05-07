17726aa0b677392ba62cad725a5650ee7c2ca785
Yusuf-bek <ogabeksherakhmatov@gmail.com>
2024-05-07 10:37:33 +0500

    unary operators

---
+++ Values/unary_operators.js
@@ -0,0 +1,16 @@
+// Unary Operators
+
+
+console.log(typeof 3);
+// => integer
+
+console.log(typeof "Lalalalal");
+// => string
+
+// typeof is unary operator
+
+
+//* Operators does some operations 
+
+
+
