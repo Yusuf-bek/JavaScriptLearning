088c8ec957ac6958fd0351e863788231a2e38dd9
Yusuf-bek <ogabeksherakhmatov@gmail.com>
2024-05-07 09:38:11 +0500

    backsticks, quotes

---
+++ Values/backticks.js
@@ -0,0 +1,15 @@
+console.log("First line \n Secons line");
+
+console.log(`This is for operations like ${4 + 5}`);
+
+console.log(`This is for operations like \`\${4 + 5}\``);
+
+// one slash (/) is for ignoring command afterwards so you can represent 
+// character after it
+
+// or you can switch quotes
+
+console.log('Switch quotes "Boom"');
+console.log("Switch quotes 'Boom'");
+
+
