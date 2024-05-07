ae462d9d1879955a5f71fc1fb7e8e9edfe52abc1
Yusuf-bek <ogabeksherakhmatov@gmail.com>
2024-05-07 07:49:42 +0500

    First commit (simple practices)

---
+++ firstCodes/firstCodes.js
@@ -0,0 +1,10 @@
+console.log('anyDataHere i do not what im talking about');
+
+let int, i;
+
+i = 3;
+
+console.log("i: ",i);
+console.log("bu yerda i variable ning bir biriga qo'shilgani: ",i + i);
+console.log("i ni 4 ga ko'paytirilgani: ",i * 4);
+console.log("i dan 2003 olib tashlangani: ", i - 2003);
---
+++ firstCodes/index.js
@@ -0,0 +1,11 @@
+console.log('hello from index.js');
+var i = (i + i - i * i + i + i + i);
+console.log(typeof i);
+
+i = 4;
+
+console.log(typeof i);
+
+console.log(i);
+
+console.log(typeof undeclaredVariable);
