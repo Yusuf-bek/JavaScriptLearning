953e66982dd17e9a226c694ef19a36164bb02e46
Yusuf-bek <ogabeksherakhmatov@gmail.com>
2024-05-07 17:54:15 +0500

    emptyValues

---
+++ Values/emptyValues.js
@@ -0,0 +1,32 @@
+// Null 
+
+let someThing = null;
+
+let someOtherThing;
+
+console.log(typeof someThing);
+//=> Object
+
+console.log(typeof someOtherThing);
+//=> Undefined
+
+// As you can see Null is a value
+// Undefined is type  
+
+
+// lets print each variable value
+
+console.log(someThing)
+
+console.log(someOtherThing)
+
+// so null remains as value
+// But undefined is also can be value
+
+
+let someVariable = NaN;
+
+console.log(someVariable);
+console.log(typeof someVariable);
+
+console.log(Number.isNaN('nimadurla'));
