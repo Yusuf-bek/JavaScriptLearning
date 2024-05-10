a2e22c775a2ce4e1d7a284313bbdabf4677d91a7
Yusuf-bek <ogabeksherakhmatov@gmail.com>
2024-05-10 21:01:57 +0500

    just some codes

---
+++ programStructure/just_codes.js
@@ -0,0 +1,44 @@
+let caught = 3 * 4
+let caught2 = 3
+
+let myString = 3
+
+myString = "Hello"
+
+console.log(myString)
+
+const myValues = "LoL"
+
+console.log(myValues)
+
+// Javascript is dynamically typed language
+// like python
+// and its scripting (the way codes written) similar to c and dart
+
+
+let myFrinedsDebt = 300000
+
+myFrinedsDebt = myFrinedsDebt - 10000
+
+console.log(myFrinedsDebt)
+
+
+let one = 1, two = 2, five = 5
+console.log(one + two + five)
+
+
+
+var myName = "Og'abek"
+const greeting = "Assalamu alaykum"
+
+console.log(greeting,myName)
+// when you use , when printing stirings 
+// because of , one space will be added between them
+
+
+var myOtherName = "Yusuf"
+const otherGreeting = "Hello "
+
+console.log(otherGreeting + myOtherName)
+// here + is just adding to strings to each other
+// not adding any space between them
